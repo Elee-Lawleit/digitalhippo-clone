6d575fd08d168fb31c06f19311939e2333428f72
Elee Lawleit <alibutt200235@gmail.com>
2023-12-12 23:14:20 +0500

    set up email verification /w resend, pretend it's working

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -8,6 +8,17 @@ dotenv.config({
     path: path.resolve(__dirname, "../.env")
 })
 
+//setting up email service through resend and nodemailer
+const transporter = nodemailer.createTransport({
+    host: "smtp.resend.com",
+    secure: true,
+    port: 465,
+    auth: {
+        user: "resend",
+        pass: process.env.RESEND_EMAIL_API_KEY
+    }
+})
+
 let cached = (global as any).payload 
 
 if(!cached){
@@ -33,6 +44,12 @@ export const getPayloadClient = async ({initOptions} : Args = {}): Promise<Paylo
     if(!cached.promise){
         cached.promise = payload.init({
             secret: process.env.PAYLOAD_SECRET,
+            email: {
+                transport: transporter,
+                //can change this to custom domain later
+                fromAddress: "alibutt200235@gmail.com", 
+                fromName: "DigitalHippo"
+            },
             local: initOptions?.express ? false: true,
             ...(initOptions || {})
         })
--- src/trpc/routers/authRouter.ts
+++ src/trpc/routers/authRouter.ts
@@ -6,8 +6,6 @@ import { publicProcedure, router } from "../trpc"
 export const authRouter = router({
     craetePayloadUser: publicProcedure.input(AuthCredentialsValidator).mutation(async ({ input }) => {
         const { email, password } = input
-
-        console.log("DATA: ", email," ", password)
         const payload = await getPayloadClient()
 
         const { docs: users } = await payload.find({
